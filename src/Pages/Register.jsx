7d5afb681e45ff521faef9c5fa4de578228f24db
Jannatul <limajannat1675@gmail.com>
2025-05-27 08:37:58 +0600

    setup firebase project

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -3,6 +3,16 @@ import Lottie from 'lottie-react';
 import lottieImage from '../assets/lottiefiles/Register.json';
 
 const Register = () => {
+    // for getting form data
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        
+
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -13,7 +23,7 @@ const Register = () => {
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className='text-5xl font-bold'>Register Now!</h1>
-        <form>
+        <form onSubmit={handleRegister}>
             <fieldset className="fieldset">
           <label className="label">Email</label>
           <input type="email" name='email' className="input" placeholder="Email" />
